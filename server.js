c9474ec4db6055e03ed44dcf688deffb4fbd2680
Erin Knapp <erin.knapp@gmail.com>
2022-03-11 19:20:05 -0500

    fixing server.js code

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 
-const PORT = process.env.port || 3001;
-//defining the routes needed from backend
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+// files where the routes are
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.urlencoded({ extended: true}));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// linking to routes to front end
+// connecting the routes to the frontend
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(PORT, () =>{
-    console.log('Server now on port 3001');
+app.listen(PORT, () => {
+    console.log('Server now on Port 3001');
 });
